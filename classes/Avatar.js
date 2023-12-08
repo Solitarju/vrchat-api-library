c31278826c14502306b0eead61d7d03737f85505
Solitarju <72232630+Solitarju@users.noreply.github.com>
2023-12-08 23:44:17 +0000

    Avatar Class

---
+++ classes/Avatar.js
@@ -0,0 +1,28 @@
+class Avatar {
+
+    assetUrl = "";
+    assetUrlObject = {};
+    authorId = "";
+    authorName = "";
+    created_at = "";
+    description = "";
+    featured = false;
+    id = "";
+    imageUrl = "";
+    name = "";
+    releaseStatus = "";
+    tags = [""];
+    thumbnailImageUrl = "";
+    unityPackageUrl = "";
+    unityPackages = [{"assetUrl":"","assetUrlObject":{},"assetVersion":0,"created_at":"","id":"","platform":"","pluginUrl":"","pluginUrlObject":{},"unitySortNumber":0,"unityVersion":""}];
+    updated_at = "";
+    version = 0;
+
+    constructor(res = {}) {
+        if(!Object.keys(res).length > 0) return this;
+        Object.keys(res).forEach(element => {
+            this[element] = res[element];
+        });
+    };
+
+}
