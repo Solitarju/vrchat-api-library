5026687ff8b148c171d441f48b6e05e8ab51de7d
Solitarju <72232630+Solitarju@users.noreply.github.com>
2024-02-01 15:36:10 +0000

    FileUpload object

---
+++ classes/FileUpload.js
@@ -0,0 +1,14 @@
+class FileUpload {
+
+    url = "";
+
+    constructor(res = {}) {
+        if(!Object.keys(res).length > 0) return this;
+        Object.keys(res).forEach(element => {
+            this[element] = res[element];
+        });
+    }
+
+}
+
+module.exports = { FileUpload };
