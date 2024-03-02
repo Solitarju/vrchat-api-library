7451cb2ef998c122d70a8e9714edbcc07fb251bf
Solitarju <72232630+Solitarju@users.noreply.github.com>
2024-03-02 10:30:32 +0000

    Refactored DeleteGroupAnnouncement function

--- classes/APIs/GroupsApi.js
+++ classes/APIs/GroupsApi.js
@@ -226,16 +226,19 @@ class GroupsApi {
      * 
      * Deletes the announcement for a Group.
      * 
-     * @returns {Promise<JSON>} 
+     * @param {string} groupId
+     * 
+     * @returns {Promise<Success>} Returns a single Success object. 
      */
-    async DeleteGroupAnnouncement(groupId = "") {
-        if(!this.#authCookie) return { success: false, status: 401 };
-        if(!groupId) return { success: false, status: 400 };
+    async DeleteGroupAnnouncement(groupId) {
+        if(!this.#authCookie) return new Error("Invalid Credentials", 401, {});
+        if(!groupId) return new Error("Required Argument(s): groupId", 400, {});
 
         const res = await this.#fetch(`${this.#APIEndpoint}/groups/${groupId}/announcement`, { method: 'DELETE', headers: this.#GenerateHeaders(true) });
-        if(!res.ok) return { success: false, status: res.status };
+        const json = await res.json();
 
-        return { success: true, res: await res.json() };
+        if(!res.ok) return new Error(json.error?.message ?? "", res.status, json);
+        return new Success(json);
     }
 
     /**
